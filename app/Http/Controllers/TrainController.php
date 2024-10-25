f748b22d71e2f5cdf08c9e40b588aeac5f006cfc
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-25 19:43:04 +0200

    added data to trains in my db

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
     //gestisco la pagina index
     public function index(){
+        // recupero i dati train dal db e li gestisco
+        $trains = Train::all();
+        dd($trains);
         return view('trains.index');
     }
 }
