89c4af447ef03b0de6f561d3f378f0ab42bed01d
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-25 22:11:38 +0200

    added links to navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,17 @@
 <header>
-    <nav class="navbar bg-light">
-        <div class="container">
-          <a class="navbar-brand" href="#">
-            <img class="text-success-emphasis train-logo" src="{{Vite::asset("resources/img/icon-train.png")}}" alt="Bootstrap">
-          </a>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary text-black">
+        <div class="container-fluid">
+            <img class="text-success-emphasis train-logo" src="{{ Vite::asset('resources/img/icon-train.png') }}" alt="Bootstrap">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Trains</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </nav>
+    </nav>
 </header>
