6e359dd844fc65f933b44fcc6a496e797e9a0cb5
GinaTovarAlmario <ginatovaralmario@gmail.com>
2024-10-28 18:00:01 +0100

    added route and displayed in page

--- resources/views/passengers/index.blade.php
+++ resources/views/passengers/index.blade.php
@@ -2,7 +2,44 @@
 
 @section('page-title', 'Passengers Index')
 @section('main-content')
-    <h1>
-        passengers list
-    </h1>
+    <section id="passengers" class="bg-warning-subtle">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1 class="mt-3 mb-4">
+                        Paasengers List
+                    </h1>
+                </div>
+                <div class="row">
+                    @forelse ($passengers as $passenger)
+                        <div class="col-4 mt-3 mb-3">
+                            <div class="card box bg-success-subtle">
+                                <div class="card-body">
+                                    <h5 class="card-title">
+                                        NAME: {{ $passenger->nome}}
+                                    </h5>
+                                    <h5 class="card-title mb-2 text-body-secondary">
+                                        LASTNAME: {{ $passenger->cognome}}
+                                    </h5>
+                                    <p class="card-text">
+                                        DATE OF BIRTH: {{ $passenger->data_nascita}}
+                                    </p>
+                                    <p class="card-text">
+                                        CLASS: {{ $passenger->classe }}
+                                    </p>
+                                    <p class="card-text">
+                                        PASSENGER CODE: {{ $passenger->numero_passeggero }}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <h2>
+                            No Passangers available at the moment
+                        </h2>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PassengerController;
 use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class,"home"]);
 
-Route::get('/trains', [TrainController::class,"index"]);
+Route::get('/trains', [TrainController::class,"index"])->name('trains.index');
+Route::get('/passengers', [PassengerController::class,"index"])->name('passenger.index');
+
